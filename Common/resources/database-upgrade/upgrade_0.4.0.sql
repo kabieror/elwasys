00ec206436228e79dc72fffd05215ea44bbce212
Oliver Kabierschke <code@oliver-kabierschke.de>
2023-06-21 17:26:44 +0200

    Update database permissions

--- Common/resources/database-upgrade/upgrade_0.4.0.sql
+++ Common/resources/database-upgrade/upgrade_0.4.0.sql
@@ -11,4 +11,7 @@ ALTER COLUMN fhem_power_name SET DEFAULT '';
 ALTER TABLE devices
 ADD COLUMN deconz_uuid VARCHAR(64) DEFAULT '';
 
+GRANT UPDATE ON devices TO GROUP elwaclients;
+
 UPDATE config SET value='0.4.0' WHERE key='db.version';
+
