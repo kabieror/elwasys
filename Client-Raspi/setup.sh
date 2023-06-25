d47840cc49acc9d3a0bb977982d00e823f3af522
Oliver Kabierschke <code@oliver-kabierschke.de>
2023-06-25 11:29:25 +0200

    Install deconz

--- Client-Raspi/setup.sh
+++ Client-Raspi/setup.sh
@@ -23,88 +23,145 @@ function generate_password() {
   echo "$password"
 }
 
-echo
-echo
-echo === Database Connection ===
-echo
-read -p "Enter database server address (e.g., localhost:5432): " db_server
-echo
-read -p "Enter database name: " db_name
-echo
-read -p "Enter database username: " db_user
-echo
-read -s -p "Enter database password: " db_password
-echo
-echo
-read -p "Should the database connection use SSL? (true/false): " db_use_ssl
-echo
-echo "Please enter the CA certificate for verifying the server SSL certificate."
-echo "Provide a file in PEM format."
-echo "When you're done, type #"
-read -d '#' db_ca_cert
+function collect_data() {
+    echo
+    echo
+    echo === Database Connection ===
+    echo
+    read -p "Enter database server address (e.g., localhost:5432): " db_server
+    echo
+    read -p "Enter database name: " db_name
+    echo
+    read -p "Enter database username: " db_user
+    echo
+    read -s -p "Enter database password: " db_password
+    echo
+    echo
+    read -p "Should the database connection use SSL? (true/false): " db_use_ssl
+    echo
+    echo "Please enter the CA certificate for verifying the server SSL certificate."
+    echo "Provide a file in PEM format."
+    echo "When you're done, type #"
+    read -d '#' db_ca_cert
 
-echo
-echo
-echo === Email Settings ===
-echo
-read -p "Enter SMTP server: " smtp_server
-echo
-read -p "Enter SMTP port: " smtp_port
-echo
-read -p "Enter SMTP username: " smtp_user
-echo
-read -s -p "Enter SMTP password: " smtp_password
-echo
-echo
-read -p "Should the SMTP connection use SSL? (true/false): " smtp_use_ssl
-echo
-read -p "Enter SMTP sender address: " smtp_sender
+    echo
+    echo
+    echo === Email Settings ===
+    echo
+    read -p "Enter SMTP server: " smtp_server
+    echo
+    read -p "Enter SMTP port: " smtp_port
+    echo
+    read -p "Enter SMTP username: " smtp_user
+    echo
+    read -s -p "Enter SMTP password: " smtp_password
+    echo
+    echo
+    read -p "Should the SMTP connection use SSL? (true/false): " smtp_use_ssl
+    echo
+    read -p "Enter SMTP sender address: " smtp_sender
 
-echo
-echo
-echo === elwasys Configuration ===
-echo
-read -p "Enter client location: " location
-echo
-read -p "Enter portal URL: " portal_url
-echo
-echo
-echo
+    echo
+    echo
+    echo === elwasys Configuration ===
+    echo
+    read -p "Enter client location: " location
+    echo
+    read -p "Enter portal URL: " portal_url
+    echo
+    echo
+    echo
+}
 
-echo "DB: $db_name // SMTP: $smtp_server // SSH: $ssh_password"
-echo "$db_ca_cert"
+function install_dependencies() {
+    sudo apt-get update
+    sudo apt-get install -y curl jq
+}
 
-log_state Starting Installation...
+# # # # # # #
+function install_java() {
+    log_state Installing Java Runtime Environment...
+    wget -q -O - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
+    echo "deb [arch=arm64] https://apt.bell-sw.com/ stable main" | sudo tee /etc/apt/sources.list.d/bellsoft.list
+    sudo apt-get update
+    sudo apt-get install -y bellsoft-java17-runtime-full
+}
 
-echo -e "\n > Installing Java Runtime Environment"
-wget -q -O - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
-echo "deb [arch=arm64] https://apt.bell-sw.com/ stable main" | sudo tee /etc/apt/sources.list.d/bellsoft.list
-sudo apt update
-sudo apt install bellsoft-java17-runtime-full
+
+# # # # # # #
+function setup_firewall() {
+    log_state Installing Firewall...
+
+    # Install UFW
+    sudo apt-get update
+    sudo apt-get install -y ufw
+
+    # Enable UFW
+    sudo ufw enable
+
+    sudo ufw default deny incoming
+    sudo ufw allow 22
+    sudo ufw --force enable
+    sudo ufw status
+}
 
 
+# # # # # # #
+function install_deconz() {
+    log_state Installing deCONZ Zigbee Gateway...
 
-log_state Installing elwasys...
-ELWA_ROOT=/opt/elwasys
-sudo mkdir -p $ELWA_ROOT
-sudo chown "$USER:$USER" $ELWA_ROOT
+    # Validations
+    [ "$DECONZ_PASSWORD" ] || { echo "DECONZ_PASSWORD must be set"; exit 1; }
 
-cd $ELWA_ROOT
+    sudo gpasswd -a $USER dialout
+    wget -O - http://phoscon.de/apt/deconz.pub.key | \
+            sudo apt-key add -
+    sudo sh -c "echo 'deb http://phoscon.de/apt/deconz \
+                $(lsb_release -cs) main' > \
+                /etc/apt/sources.list.d/deconz.list"
 
-VER=$(curl --silent -qI https://github.com/kabieror/elwasys/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')
-jar_file=./raspi-client-${VER}.jar
-if [ ! -f "$jar_file" ]
-then
-    wget https://github.com/kabieror/elwasys/releases/download/$VER/raspi-client-${VER}.jar -O $jar_file
-    ln -s ./raspi-client-${VER}.jar ./raspi-client.latest.jar
-else
-    echo "Skipping downloading raspi-client JAR. File already exists: $jar_file"
-fi
+    sudo apt-get update
+    sudo apt-get install -y deconz
 
-log_state Configuring elwasys
-# Populate the Config file
-config_file="./elwasys.properties"
-sudo tee "$config_file" > /dev/null <<EOT
+    otp="elwasysinstalleronetimetoken"
+    curl -XPOST \
+        -H 'Authorization: Basic ZGVsaWdodDpkZWxpZ2h0' \
+        -H "Content-type: application/json" \
+        -d "{ \"username\": \"$otp\", \"devicetype\": \"installer\" }" \
+        http://localhost/api
+    old_hash=`echo "$dc_user:$dc_user" | base64`
+    new_hash=`echo "$dc_user:$DECONZ_PASSWORD" | base64`
+    curl -XPUT \
+        -H "Content-type: application/json" \
+        -d "{ \"username\": \"$dc_user\", \"oldhash\": \"$old_hash\", \"newhash\": \"$new_hash\" }" \
+        http://localhost/api/$otp/config/password
+}
+
+# # # # # # #
+function install_elwasys() {
+    log_state Installing elwasys...
+    sudo mkdir -p $ELWA_ROOT
+    sudo chown "$USER:$USER" $ELWA_ROOT
+
+    cd $ELWA_ROOT
+
+    VER=$(curl --silent -qI https://github.com/kabieror/elwasys/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')
+    jar_file=./raspi-client-${VER}.jar
+    if [ ! -f "$jar_file" ]
+    then
+        wget https://github.com/kabieror/elwasys/releases/download/$VER/raspi-client-${VER}.jar -O $jar_file
+        ln -s ./raspi-client-${VER}.jar ./raspi-client.latest.jar
+    else
+        echo "Skipping downloading raspi-client JAR. File already exists: $jar_file"
+    fi
+}
+
+# # # # # # #
+function config_elwasys() {
+    log_state Configuring elwasys
+    # Populate the Config file
+    config_file="./elwasys.properties"
+    sudo tee "$config_file" > /dev/null <<EOT
 # The address of the postgresql server
 database.server: $db_server
 
@@ -135,15 +192,15 @@ smtp.password: $smtp_password
 smtp.useSSL: $smtp_use_ssl
 smtp.senderAddress: $smtp_sender
 EOT
-sudo chmod 600 "$config_file"
+    sudo chmod 600 "$config_file"
 
-# configure logging
-logback_config="./logback.xml"
-tee "$logback_config" > /dev/null <<EOT
+    # configure logging
+    logback_config="./logback.xml"
+    tee "$logback_config" > /dev/null <<EOT
 <configuration scan="true" debug="true">
     <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
         <filter class="ch.qos.logback.classic.filter.ThresholdFilter">
-              <level>ERROR</level>
+            <level>ERROR</level>
         </filter>
         <encoder>
             <pattern>%d %level %logger{36} - %msg%n</pattern>
@@ -163,7 +220,7 @@ tee "$logback_config" > /dev/null <<EOT
     </appender>
     <appender name="FILE" class="ch.qos.logback.core.rolling.RollingFileAppender">
         <filter class="ch.qos.logback.classic.filter.ThresholdFilter">
-              <level>INFO</level>
+            <level>INFO</level>
         </filter>
         <rollingPolicy class="ch.qos.logback.core.rolling.TimeBasedRollingPolicy">
             <!-- daily rollover -->
@@ -185,19 +242,19 @@ tee "$logback_config" > /dev/null <<EOT
 </configuration>
 EOT
 
-# create ca-db.pem
-ca_db="./ca-db.pem"
-echo -e "$db_ca_cert" > $ca_db
+    # create ca-db.pem
+    ca_db="./ca-db.pem"
+    echo -e "$db_ca_cert" > $ca_db
 
-truststore_password=$(generate_password)
-truststore_file="./.truststore"
-# Remove truststore file if it already exists
-[ -f "$truststore_file" ] && rm -f $truststore_file
-sudo keytool -import -trustcacerts -keystore "$truststore_file" -storepass "$truststore_password" -alias ca_cert -file "$ca_db" -noprompt
+    truststore_password=$(generate_password)
+    truststore_file="./.truststore"
+    # Remove truststore file if it already exists
+    [ -f "$truststore_file" ] && rm -f $truststore_file
+    sudo keytool -import -trustcacerts -keystore "$truststore_file" -storepass "$truststore_password" -alias ca_cert -file "$ca_db" -noprompt
 
-# run.sh script
-run_script="./run.sh"
-tee "$run_script" > /dev/null <<EOT
+    # run.sh script
+    run_script="./run.sh"
+    tee "$run_script" > /dev/null <<EOT
 #!/bin/bash
 
 sudo killall java 2> /dev/null
@@ -206,17 +263,37 @@ java -Djavafx.platform=gtk -Dlogback.configurationFile=$ELWA_ROOT/logback.xml \
         -Djavax.net.ssl.trustStore=$ELWA_ROOT/.truststore -Djavax.net.ssl.trustStorePassword=$truststore_password \
         -jar raspi-client.latest.jar -verbose > log/stdout 2> log/errout
 EOT
-chmod +x "$run_script"
+    chmod +x "$run_script"
 
-# Create log output folder
-mkdir -p ./log
+    # Create log output folder
+    mkdir -p ./log
 
-# Auto-Start
-cat > ~/.xsession <<EOT
+    # Auto-Start
+    cat > ~/.xsession <<EOT
 cd $ELWA_ROOT
 ./run.sh
 EOT
+}
 
+# collect_data
+
+ELWA_ROOT=/opt/elwasys
+DECONZ_PASSWORD=$(generate_password)
+
+log_state Starting Installation...
+install_dependencies
+
+# setup_firewall
+
+# install_java
+
+install_deconz
+
+# install_elwasys
+
+# config_elwasys
+
+# # # # # # #
 echo
 echo
 log_success Installation completed!
